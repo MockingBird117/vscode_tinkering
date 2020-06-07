2ff0572e434ca447a41459ba23712ba9d65928eb
MockingBird117 <noah.vasatko@gmail.com>
2020-06-07 18:11:27 +0200

    Update hello.py

--- .vscode/hello.py
+++ .vscode/hello.py
@@ -1,33 +1,34 @@
+try:
+    #get input
 
-#get input
+    one = int(input('1st: '))
+    two = int(input('2nd: '))
 
-one = int(input('1st: '))
-two = int(input('2nd: '))
+    #choose opperant
 
-#choose opperant
+    op = input('Chose an opperand: ')
 
-op = input('Chose an opperand: ')
+    #check for opperand
 
-#check for opperand
+    if(op == '+' ):
+        solution = (one + two)
 
-if(op == '+' ):
-    solution = (one + two)
+    if(op == '-'):
+        solution = (one - two)
 
-if(op == '-'):
-    solution = (one - two)
+    if(op == '*'):
+        solution = (one * two)
 
-if(op == '*'):
-    solution = (one * two)
+    if (op == '/'):
+        solution = (one / two)
 
-if (op == '/'):
-    solution = (one / two)
+    #see if not an operand / print solution
 
-#see if not an operand
-else:
-    print('Sorry, this is not an opperand.')
+    if not op in ['+', '-', '*', '/']:
+        print('Sorry, this is not an opperand.')
+    else:
+        print("The solution is: " + str(solution))
 
-
-#print solution
-if(op == '+', '-', '*', '/'):
-    print(solution)
+except:
+    print('Pls insert integer.')
 
